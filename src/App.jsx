5053b68bc265523a326c51af1b0af48970c3b54c
ageniuscoder <23bcs059@nith.ac.in>
2025-09-08 11:32:08 +0530

    initial config

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { ChatProvider } from './contexts/ChatContext';
+import { WebSocketProvider } from './contexts/WebSocketContext';
+
+// Components
+import Layout from './components/common/Layout';
+import LoginPage from './components/auth/LoginPage';
+import SignupPage from './components/auth/SignupPage';
+import ForgotPasswordPage from './components/auth/ForgotPasswordPage';
+import ChatLayout from './components/chat/ChatLayout';
+import ProfilePage from './components/profile/ProfilePage';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route 
+            path="/login" 
+            element={
+              <Layout requireAuth={false}>
+                <LoginPage />
+              </Layout>
+            } 
+          />
+          <Route 
+            path="/signup" 
+            element={
+              <Layout requireAuth={false}>
+                <SignupPage />
+              </Layout>
+            } 
+          />
+          <Route 
+            path="/forgot-password" 
+            element={
+              <Layout requireAuth={false}>
+                <ForgotPasswordPage />
+              </Layout>
+            } 
+          />
+
+          {/* Protected Routes */}
+          <Route 
+            path="/chat" 
+            element={
+              <Layout requireAuth={true}>
+                <ChatProvider>
+                  <WebSocketProvider>
+                    <ChatLayout />
+                  </WebSocketProvider>
+                </ChatProvider>
+              </Layout>
+            } 
+          />
+          <Route 
+            path="/profile" 
+            element={
+              <Layout requireAuth={true}>
+                <ProfilePage />
+              </Layout>
+            } 
+          />
+
+          {/* Default redirect */}
+          <Route path="/" element={<Navigate to="/chat" replace />} />
+          <Route path="*" element={<Navigate to="/chat" replace />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
